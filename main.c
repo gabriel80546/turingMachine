efd3dc7e7cd34a3b1b205090abfbba67a98e9b2f
Gabriel Passos <gabriel80546@gmail.com>
2023-10-16 11:50:42 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 #define TAMANHO 100
 
@@ -8,13 +10,19 @@ void mostrarMaquinaTuring() {
 
 int main() {
     int i;
-    int maquina[TAMANHO] = {0};
+    char padrao = 'L';
+    char* maquina;
+    
+    maquina = (char *) malloc(sizeof(char) * TAMANHO);
 
+    for(i = 0; i < TAMANHO; i++){
+        maquina[i] = padrao;
+    }
 
     printf("Maquina de Turing\n");
 
     for(i = 0; i < TAMANHO; i++) {
-        printf("maquina[%i] = %i\n", i, maquina[i]);
+        printf("maquina[%i] = '%c'\n", i, maquina[i]);
     }
 
     return 0;
