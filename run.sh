7570daaafeddd1400dcfc38deb708e2853530c55
Gabriel Passos <gabriel80546@gmail.com>
2023-10-16 12:43:46 -0300

    ./push.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 # Desativa o modo de depuração
 PS4='+ $(date "+%Y-%m-%d %T") $BASH_SOURCE:$LINENO: '
 set -x
+set -e
 
 gcc -g main.c -o main
 valgrind -s ./main
 
 # Desativa o modo de depuração
+set +e
 set +x
