255c8083d7968208dd23344750be406af6017dc2
Gabriel Passos <gabriel80546@gmail.com>
2023-10-16 11:42:20 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
+
+#define TAMANHO 100
+
+void mostrarMaquinaTuring() {
+    return;
+}
 
 int main() {
+    int i;
+    int maquina[TAMANHO] = {0};
+
 
     printf("Maquina de Turing\n");
+
+    for(i = 0; i < TAMANHO; i++) {
+        printf("maquina[%i] = %i\n", i, maquina[i]);
+    }
 
     return 0;
 }
