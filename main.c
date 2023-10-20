4e389e954564d2c763a329112fece95cd683a2ad
Gabriel Passos <gabriel80546@gmail.com>
2023-10-20 14:44:12 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -45,6 +45,16 @@ void mostrarInstrucao(Instrucao *inst) {
     return;
 }
 
+Instrucao *initInstrucao(int lado, char conteudo, int estado, int proximoEstado) {
+    Instrucao *saida;
+    saida = (Instrucao *)malloc(sizeof(Instrucao) * 1);
+    saida->lado = lado;
+    saida->conteudo = conteudo;
+    saida->estado = estado;
+    saida->proximoEstado = proximoEstado;
+    return saida;
+}
+
 int main() {
     int i;
     char padrao = '_';
@@ -52,14 +62,16 @@ int main() {
     char* fita;
     Instrucao *a;
 
-    a = (Instrucao *)malloc(sizeof(Instrucao) * 1);
-    a->lado = 1;
-    a->conteudo = 'a';
-    a->estado = -1;
-    a->proximoEstado = 3;
-    mostrarInstrucao(a);
+    a = initInstrucao(1, 'e', -1, 3);
+    // a = (Instrucao *)malloc(sizeof(Instrucao) * 1);
+    // a->lado = 1;
+    // a->conteudo = 'e';
+    // a->estado = -1;
+    // a->proximoEstado = 3;
     fita = (char *) malloc(sizeof(char) * TAMANHO);
+
     fita = definirPeloPadrao(fita, padrao);
+    mostrarInstrucao(a);
     mostrarFitaTuring(fita);
     free(fita);
     free(a);
